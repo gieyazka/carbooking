161a834121e86148a494a70d22e9451107ce50a0
gieyazka <57186807+gieyazka@users.noreply.github.com>
2021-01-26 11:27:10 +0700

    gh page

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,6 +1,6 @@
 importScripts("https://www.gstatic.com/firebasejs/7.16.1/firebase-app.js");
 importScripts(
-    "https://www.gstatic.com/firebasejs/7.16.1/firebase-messaging.js",
+    "/carbooking/firebase-messaging.js",
 );
 // For an optimal experience using Cloud Messaging, also add the Firebase SDK for Analytics.
 importScripts(
