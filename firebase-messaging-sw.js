15e74112b1a1497e20a432c81d5d498fbf47dd16
gieyazka <57186807+gieyazka@users.noreply.github.com>
2021-01-27 13:51:49 +0700

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -9,8 +9,6 @@ importScripts(
     "https://www.gstatic.com/firebasejs/7.16.1/firebase-analytics.js",
 );
 
-// Initialize the Firebase app in the service worker by passing in the
-// messagingSenderId.
 firebase.initializeApp({
     'messagingSenderId': '851373916828',
     'apiKey': 'AIzaSyCX9MgSHZd64fwVG7I7QriHcfyLzryob08',
@@ -18,23 +16,14 @@ firebase.initializeApp({
     'appId': '1:851373916828:web:bdb3c4099248049c946359',
 });
 
-// Retrieve an instance of Firebase Messaging so that it can handle background
-// messages.
 const messaging = firebase.messaging();
-console.log(messaging);
 messaging.setBackgroundMessageHandler(function (payload) {
-    // console.log(
-    //     "[firebase-messaging-sw.js] Received background message ",
-    //     payload,
-    // );
 
-    // Customize notification here
     const notificationTitle = "Car Booking System";
     const notificationOptions = {
         body: "มีการเพิ่มงานให้คุณ.",
         icon: "/logo.png",
     };
-    console.log(self.registration);
     return self.registration.showNotification(
         notificationTitle,
         notificationOptions,
