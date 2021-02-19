ec5a999e079b6f147988a6a94a938abf25bf8a78
gieyazka <57186807+gieyazka@users.noreply.github.com>
2021-02-19 10:38:43 +0700

    edit dispatch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-import 'antd/dist/antd.css';
-import { BrowserRouter as Router, Route, Link, useLocation, Switch } from "react-router-dom";
-import DataContextProvider from './component/store/store'
+import logo from "./logo.svg";
+import "./App.css";
+import "antd/dist/antd.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  useLocation,
+  Switch,
+} from "react-router-dom";
+import DataContextProvider from "./component/store/store";
 
-import Login from './component/user/login'
-import Guard from './component/user/guard'
-import Approve from './component/user/approve'
-import Flow from './component/flow'
+import Login from "./component/user/login";
+import Guard from "./component/user/guard";
+import Approve from "./component/user/approve";
+import Flow from "./component/flow";
 
 // import FromRequest from './component/form/formrequest'
 // import Genaral from './component/general'
-import Layout from './component/layout'
+import Layout from "./component/layout";
 function App() {
   return (
-    <div style={{ fontFamily: 'Bai Jamjuree' }}>
+    <div style={{ fontFamily: "Bai Jamjuree" }}>
       <DataContextProvider>
-        <Router
-         basename={'/carbooking/#/'}
-         >
-          
+        <Router basename={"/carbooking/#/"}>
           <Switch>
             <Route exact path="/" component={Login} />
 
