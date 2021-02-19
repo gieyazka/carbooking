19df74ea195e94bd1bd64fe419eff4f4bb8f2d81
gieyazka <57186807+gieyazka@users.noreply.github.com>
2021-02-19 11:17:47 +0700

    edit dispatch

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,32 +1,92 @@
 importScripts("https://www.gstatic.com/firebasejs/7.16.1/firebase-app.js");
 importScripts(
-    // "carbooking/firebase-messaging.js",
-    "https://www.gstatic.com/firebasejs/7.16.1/firebase-messaging.js",
-
+  // "carbooking/firebase-messaging.js",
+  "https://www.gstatic.com/firebasejs/7.16.1/firebase-messaging.js"
 );
 // For an optimal experience using Cloud Messaging, also add the Firebase SDK for Analytics.
 importScripts(
-    "https://www.gstatic.com/firebasejs/7.16.1/firebase-analytics.js",
+  "https://www.gstatic.com/firebasejs/7.16.1/firebase-analytics.js"
 );
 
 firebase.initializeApp({
-    'messagingSenderId': '851373916828',
-    'apiKey': 'AIzaSyCX9MgSHZd64fwVG7I7QriHcfyLzryob08',
-    'projectId': 'car-booking-4433a',
-    'appId': '1:851373916828:web:bdb3c4099248049c946359',
+  messagingSenderId: "851373916828",
+  apiKey: "AIzaSyCX9MgSHZd64fwVG7I7QriHcfyLzryob08",
+  projectId: "car-booking-4433a",
+  appId: "1:851373916828:web:bdb3c4099248049c946359",
 });
 
 const messaging = firebase.messaging();
 messaging.setBackgroundMessageHandler(function (payload) {
-
-    const notificationTitle = "Car Booking System";
-    const notificationOptions = {
-        body: "มีการเพิ่มงานให้คุณ.",
-        icon: "/logo.png",
-    };
-    return self.registration.showNotification(
-        notificationTitle,
-        notificationOptions,
-    );
+  const notificationTitle = "Car Booking System";
+  const notificationOptions = {
+    body: "มีการเพิ่มงานให้คุณ.",
+    icon: "/logo.png",
+  };
+  return self.registration.showNotification(
+    notificationTitle,
+    notificationOptions
+  );
 });
-const initMessage = firebase.messaging()
+// const CACHE_NAME = "offline";
+// const OFFLINE_URL = "offline.html";
+
+// self.addEventListener("install", function (event) {
+//   console.log("[ServiceWorker] Install");
+
+//   event.waitUntil(
+//     (async () => {
+//       const cache = await caches.open(CACHE_NAME);
+//       // Setting {cache: 'reload'} in the new request will ensure that the response
+//       // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
+//       await cache.add(new Request(OFFLINE_URL, { cache: "reload" }));
+//     })()
+//   );
+
+//   self.skipWaiting();
+// });
+
+// self.addEventListener("activate", (event) => {
+//   console.log("[ServiceWorker] Activate");
+//   event.waitUntil(
+//     (async () => {
+//       // Enable navigation preload if it's supported.
+//       // See https://developers.google.com/web/updates/2017/02/navigation-preload
+//       if ("navigationPreload" in self.registration) {
+//         await self.registration.navigationPreload.enable();
+//       }
+//     })()
+//   );
+
+//   // Tell the active service worker to take control of the page immediately.
+//   self.clients.claim();
+// });
+
+// self.addEventListener("fetch", function (event) {
+//   // console.log('[Service Worker] Fetch', event.request.url);
+//   if (event.request.mode === "navigate") {
+//     event.respondWith(
+//       (async () => {
+//         try {
+//           const preloadResponse = await event.preloadResponse;
+//           if (preloadResponse) {
+//             return preloadResponse;
+//           }
+
+//           const networkResponse = await fetch(event.request);
+//           return networkResponse;
+//         } catch (error) {
+//           console.log(
+//             "[Service Worker] Fetch failed; returning offline page instead.",
+//             error
+//           );
+
+//           const cache = await caches.open(CACHE_NAME);
+//           const cachedResponse = await cache.match(OFFLINE_URL);
+//           return cachedResponse;
+//         }
+//       })()
+//     );
+//   }
+// });
+
+const initMessage = firebase.messaging();
--- public/testsw.js
+++ public/testsw.js
@@ -1,20 +1,61 @@
-self.addEventListener("install", function (e) {
-  e.waitUntil(
-    caches.open("pwa-example").then(function (cache) {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/app.js",
-        "/app.css", //Add any other assets your web page needs
-      ]);
-    })
+const CACHE_NAME = "offline";
+const OFFLINE_URL = "offline.html";
+
+self.addEventListener("install", function (event) {
+  console.log("[ServiceWorker] Install");
+
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      // Setting {cache: 'reload'} in the new request will ensure that the response
+      // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
+      await cache.add(new Request(OFFLINE_URL, { cache: "reload" }));
+    })()
   );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("[ServiceWorker] Activate");
+  event.waitUntil(
+    (async () => {
+      // Enable navigation preload if it's supported.
+      // See https://developers.google.com/web/updates/2017/02/navigation-preload
+      if ("navigationPreload" in self.registration) {
+        await self.registration.navigationPreload.enable();
+      }
+    })()
+  );
+
+  // Tell the active service worker to take control of the page immediately.
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", function (event) {
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
-    })
-  );
+  // console.log('[Service Worker] Fetch', event.request.url);
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      (async () => {
+        try {
+          const preloadResponse = await event.preloadResponse;
+          if (preloadResponse) {
+            return preloadResponse;
+          }
+
+          const networkResponse = await fetch(event.request);
+          return networkResponse;
+        } catch (error) {
+          console.log(
+            "[Service Worker] Fetch failed; returning offline page instead.",
+            error
+          );
+
+          const cache = await caches.open(CACHE_NAME);
+          const cachedResponse = await cache.match(OFFLINE_URL);
+          return cachedResponse;
+        }
+      })()
+    );
+  }
 });
